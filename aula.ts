ee4ca7289b65e031a458e73739aa3c45c7ee405b
JessicaSanto <jessicaesanto@outlook.com>
2023-07-21 19:58:45 -0300

    explicacao

---
+++ aula.ts
@@ -0,0 +1,55 @@
+// Instalação TypeScript -> npm i -g typescript
+// Instalação de pacote para rodar no terminal -> npm i -g ts-node
+// Para rodar no terminal-> ts-node arquivo.ts
+// Instalação de Prompt-Sync ->npm i prompt-sync
+// Rodar prompt-sync -> npm i --save-dev @types/prompt-sync
+
+
+// INFERENCIA
+let x = true;
+
+//ANNOTATION
+let y: number = 2;
+
+//EXEMPLO 1
+let z:number = 15;
+z = 20
+console.log(z)
+
+// TIPOS DE DADOS
+let nome: string = "Thiago";
+
+let idade: number = 2;
+
+const isAdmin: boolean = true;
+
+// OBJETO
+
+const user: {nome: string, idade: number} = {
+    nome: "Thiago",
+    idade: 25,
+};
+
+// TIPO ANY 
+
+let a: any = 5;
+a = "jessica"
+a = true
+
+// UNION TYPE
+
+let id: string | number = "10"
+id = 10
+
+// TYPE ALIAS
+type Anderson = number | string
+
+const Usuario: Anderson = 10
+
+// FUNÇÃO
+
+function soma(a: number,b: number) {
+    return a +b;
+}
+console.log(soma(12,12));
+
